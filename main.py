5c6106cb879331f2e639fc18661b711ab36228e7
mirateodora <ashadora154@gmail.com>
2025-11-16 15:22:59 +0200

    add rest of UI pages

--- main.py
+++ main.py
@@ -14,5 +14,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
